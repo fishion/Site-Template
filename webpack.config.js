b9537c8468b0a5a866af2db028b79b6202c2f7f7
Alex <alex@monney.me>
2023-06-25 21:28:42 +0100

    more linting

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,42 @@
-const fs = require('fs');
-const path = require('path');
-const HandlebarsPlugin = require("handlebars-webpack-plugin");
+const path = require('path')
+  , HandlebarsPlugin = require('handlebars-webpack-plugin')
 const hbx = require('HandlebarsExtended')({
   appRoot : path.resolve(__dirname),
   pagesPath : 'src/view/page',
   wrappersPath : 'src/view/components/wrappers',
   includesPath : 'src/view/components/includes',
   extension : 'html'
-});
+})
 
 module.exports = {
-  mode: 'production',
-  entry: {
-    site : './src/js/site.js',
+  mode : 'production',
+  entry : {
+    site : './src/js/site.js'
   },
-  output: {
-    filename: '[name].js',
-    path: path.join(__dirname, "dist", "js"),
+  output : {
+    filename : '[name].js',
+    path : path.join(__dirname, 'dist', 'js')
   },
-  module: {
-    rules: [
+  module : {
+    rules : [
       {
-        test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ],
-      },
-    ],
+        test : /\.css$/,
+        use : ['style-loader', 'css-loader']
+      }
+    ]
   },
-  plugins: [
+  plugins : [
     new HandlebarsPlugin({
-      entry: path.join(__dirname, "src", "view", "page", "*.hbs"),
-      output: path.join(__dirname, "dist", "[name]"),
+      entry : path.join(__dirname, 'src', 'view', 'page', '*.hbs'),
+      output : path.join(__dirname, 'dist', '[name]'),
       // globbed path to partials, where folder/filename is unique
-      partials: [
-        path.join(process.cwd(), "app", "src", "components", "*", "*.hbs")
+      partials : [
+        path.join(process.cwd(), 'app', 'src', 'components', '*', '*.hbs')
       ],
       // register custom helpers. May be either a function or a glob-pattern
-      helpers: {
-        wrap: hbx.helpers.wrap,
-        include: hbx.helpers.include,
+      helpers : {
+        wrap : hbx.helpers.wrap,
+        include : hbx.helpers.include
       // onBeforeSave: function (Handlebars, resultHtml, filename) {
       //   const compiledWrapper = Handlebars.compile(
       //     fs.readFileSync(`${path.join(appRoot, wrappersPath, wrapper)}.html.hbs`,'utf8').toString()
